c3ce88df0e945b7134efea3f32d1c83eeee37e88
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-02-28 14:59:22 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,40 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	}
 
+	public static Boolean isPalindrome(String word) {
+		word = word.replaceAll(" ", "").toLowerCase();
+		String[] wordArr = word.split("");
+
+		for (int i = 0; i < wordArr.length; i++) {
+			if (!wordArr[i].equals(wordArr[wordArr.length - i - 1])) {
+				return false; 
+			}
+		}
+		return true;
+	}
+
+	public static int getAge(String[] names, int[] ages, String name) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(name)) {
+                return ages[i]; 
+            }
+        }
+        return -1;
+    }
+
+	public static int countWords(String word, char character) {
+		String[] wordArray = word.split(" ");
+		int count = 0;
+
+		for (int i = 0; i < wordArray.length; i++) {
+			if (wordArray[i].indexOf(character) != -1) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
+
+
